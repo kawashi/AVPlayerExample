54fa9214d504488b2114480cf33830e24ef4c162
Marihuana <aisatu_nomahou@yahoo.co.jp>
2018-08-10 02:33:29 +0900

    first commit

--- AVExample/ViewController.swift
+++ AVExample/ViewController.swift
@@ -1,25 +1,33 @@
-//
-//  ViewController.swift
-//  AVExample
-//
-//  Created by YakisobaMelonpan on 2018/08/10.
-//  Copyright © 2018年 YakisobaMelonpan. All rights reserved.
-//
-
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    @IBAction func playVideo(_ sender: Any) {
+        
+        guard let url = URL(string: "http://www.gomplayer.jp/img/sample/mp4_h264_aac.mp4") else {
+            return
+        }
+        
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: url)
+        
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        let controller = AVPlayerViewController()
+        controller.player = player
+        
+        // Modally present the player and call the player's play() method when complete.
+        present(controller, animated: true) {
+            player.play()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
